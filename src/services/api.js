66fa8b5b46222f16b92763d277bc7ce03f3076c5
Vinicio Valdez <viniciovaldez@MacBook-Air-de-Vinicio.local>
2025-07-10 14:47:32 -0600

    correccion de url de render

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://backend-tikbal.onrender.com/api',
 });
 
 // ✅ Cargar token desde localStorage al inicializar
